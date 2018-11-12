7400a4b4ee0ad34a2dd511b4cade9dff31afbff4
Rui.Duan1 <Rui.Duan1@geely.com>
2018-11-12 09:27:55 +0800

    【对接jira】

---
+++ src/test/java/com/geely/devops/aoptest/jsontest/ReadJson.java
@@ -0,0 +1,21 @@
+package com.geely.devops.aoptest.jsontest;
+
+import net.minidev.json.JSONArray;
+import org.apache.logging.log4j.core.util.FileUtils;
+
+import java.io.File;
+
+/**
+ * @ProjectName: aop-test
+ * @Package: com.geely.devops.aoptest.jsontest
+ * @Description: java类作用描述
+ * @Author: duanrui
+ * @CreateDate: 2018-09-11 14:17
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2018
+ */
+public class ReadJson {
+
+
+}
